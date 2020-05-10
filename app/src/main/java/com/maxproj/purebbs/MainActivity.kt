426ac45e36b9dd0268460488bf40dd3d3d78828a
maxyou <hyyou2010@gmail.com>
2020-05-10 21:37:18 +0800

    toolbar up button call out navi-view

--- app/src/main/java/com/maxproj/purebbs/MainActivity.kt
+++ app/src/main/java/com/maxproj/purebbs/MainActivity.kt
@@ -3,8 +3,18 @@ package com.maxproj.purebbs
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration : AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -12,5 +22,28 @@ class MainActivity : AppCompatActivity() {
 
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
+
+        val host: NavHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment? ?: return
+
+        val navController = host.navController
+
+        val drawerLayout : DrawerLayout? = findViewById(R.id.drawer_layout)
+
+        appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.home_dest),
+            drawerLayout)
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        val sideNavView = findViewById<NavigationView>(R.id.nav_view)
+        sideNavView?.setupWithNavController(navController)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+//        // Allows NavigationUI to support proper up navigation or the drawer layout
+//        // drawer menu, depending on the situation
+        return findNavController(R.id.nav_host_fragment).navigateUp(appBarConfiguration)
     }
 }
