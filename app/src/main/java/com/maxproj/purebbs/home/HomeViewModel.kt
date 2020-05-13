b28c07755197d3ced4d16ff711cc3f017e419578
maxyou <hyyou2010@gmail.com>
2020-05-13 19:31:15 +0800

    data-binding fun works, but no auto refresh

--- app/src/main/java/com/maxproj/purebbs/home/HomeViewModel.kt
+++ app/src/main/java/com/maxproj/purebbs/home/HomeViewModel.kt
@@ -1,11 +1,23 @@
 package com.maxproj.purebbs.home
 import android.util.Log
 import android.view.View
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.navigation.Navigation
 import kotlinx.coroutines.withContext
 
 class HomeViewModel : ViewModel() {
+
+    private var _replyNum = MutableLiveData<Int?>(0)
+    val replyNum:LiveData<Int?>
+        get() = _replyNum
+
+    fun onClickIncReplyNum(){
+        Log.d("PureBBS", "onClickIncReplyNum")
+        _replyNum.value = _replyNum.value?.plus(1)
+        println(_replyNum.value.toString())
+    }
 
     var category:String = "not used"
 
