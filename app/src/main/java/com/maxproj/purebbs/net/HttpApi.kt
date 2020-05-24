9bb6c7bd500f75c5dd8e03e91af4fc7c8bbfae7f
maxyou <hyyou2010@gmail.com>
2020-05-25 00:51:53 +0800

    retrofit's Response

--- app/src/main/java/com/maxproj/purebbs/net/HttpApi.kt
+++ app/src/main/java/com/maxproj/purebbs/net/HttpApi.kt
@@ -1,6 +1,7 @@
 package com.maxproj.purebbs.net
 
 import retrofit2.Call
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
@@ -63,5 +64,10 @@ interface HttpApi {
     suspend fun getJsonUserById(
         @Query("jsonUserId") tab: String
     ): HttpData.User
+    @GET("jsonUserId")
+
+    suspend fun getJsonUserByIdR(
+        @Query("jsonUserId") tab: String
+    ): Response<HttpData.User>
 
 }
